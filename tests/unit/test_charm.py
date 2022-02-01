3764d6c4662994cf3e8a62254c577c132dc74487
Dominik Fleischmann <dominik.fleischmann@canonical.com>
2022-02-01 13:50:13 +0100

    Removing action and config unit tests

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -18,24 +18,6 @@ class TestCharm(unittest.TestCase):
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
 
-    def test_config_changed(self):
-        self.assertEqual(list(self.harness.charm._stored.things), [])
-        self.harness.update_config({"thing": "foo"})
-        self.assertEqual(list(self.harness.charm._stored.things), ["foo"])
-
-    def test_action(self):
-        # the harness doesn't (yet!) help much with actions themselves
-        action_event = Mock(params={"fail": ""})
-        self.harness.charm._on_fortune_action(action_event)
-
-        self.assertTrue(action_event.set_results.called)
-
-    def test_action_fail(self):
-        action_event = Mock(params={"fail": "fail this"})
-        self.harness.charm._on_fortune_action(action_event)
-
-        self.assertEqual(action_event.fail.call_args, [("fail this",)])
-
     def test_httpbin_pebble_ready(self):
         # Check the initial Pebble plan is empty
         initial_plan = self.harness.get_container_pebble_plan("httpbin")
